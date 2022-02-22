d39690e364ddcd2102a861b64a30acd58135009b
EvaNuezQ <EvaNuezQ@github.com>
2022-02-22 16:49:36 +0100

    Install mocha

---
+++ test/test.js
@@ -0,0 +1,6 @@
+import { expect } from "chai"
+describe('Array', function () {
+    it('should return -1 when the value is not present', function () {
+      expect([1, 2, 3].indexOf(4)).to.equal(-1)
+    }); 
+});
