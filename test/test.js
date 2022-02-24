c80fcd8a89371fb8f2c020468f86644113e09a94
Sara <kengork@gmail.com>
2022-02-24 16:47:41 +0100

    WIP add jsdom scripts management

--- test/test.js
+++ test/test.js
@@ -3,14 +3,14 @@ import { JSDOM } from "jsdom"
 
 describe('Index', function () {
   before(function() {
-    return JSDOM.fromFile('index.html')
+    return JSDOM.fromFile('index.html', {runScripts: 'dangerously'})
       .then((dom) => {
         global.window = dom.window;
         global.document = window.document;
       });
   })
 
-  it ('should has a title', function () {
+  it('should has a title', function () {
     expect(document.getElementsByTagName('h1')[0].innerHTML).to.equal('Hello World!');
   });
 });
