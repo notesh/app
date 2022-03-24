ead1fbf914797d6bc3d3c5b40d43593a3223520a
Mario Sanz <mariosanz92@gmail.com>
2022-03-24 17:03:41 +0100

    add to get element by text in unit test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,79 +1,74 @@
-import { expect } from "chai"
-import { JSDOM } from "jsdom"
-import userEvent from "@testing-library/user-event"
+import { expect } from 'chai';
+import { JSDOM } from 'jsdom';
+import userEvent from '@testing-library/user-event';
 
 describe('App', () => {
   beforeEach(() => {
-    return JSDOM.fromFile('index.html', {runScripts: 'dangerously', url: 'http://localhost'})
-      .then((dom) => {
-        global.window = dom.window;
-        global.document = window.document;
-        global.window.localStorage.setItem("notes", JSON.stringify(["aNote"]))
-      })
-  })
+    return JSDOM.fromFile('index.html', { runScripts: 'dangerously', url: 'http://localhost' }).then((dom) => {
+      global.window = dom.window;
+      global.document = window.document;
+      global.window.localStorage.setItem('notes', JSON.stringify(['aNote']));
+    });
+  });
 
   it('allows to write down notes', async () => {
-    const myNote = "Note"
-    const textarea = document.querySelector('textarea') 
+    const myNote = 'Note';
+    const textarea = document.querySelector('textarea');
 
-    write(myNote)
-  
-    expect(textarea.value).to.equal(myNote)
+    write(myNote);
+
+    expect(textarea.value).to.equal(myNote);
   });
 
-  it('allows to save a note', async () => {
-    const myNote = "Note"
-    write(myNote)
-    
-    save()
-    
-    const div = document.querySelector("div")
-    const notes = getSavedNotes()
-    expect(notes).to.contain(myNote)
-    expect(div.innerHTML).to.equal(myNote)
-  
+  it('clear text when save note', () => {
+    const empty = '';
+    const textarea = document.querySelector('textarea');
+    write('Note');
+
+    save();
+
+    expect(textarea.value).to.equal(empty);
   });
 
-  it('clear text when save note', ()=>{
-    const empty = ""
-    const textarea = document.querySelector("textarea")
-    write("Note")
+  it('should save multiples notes', () => {
+    const firstNote = 'first note';
+    const secondNote = 'second note';
 
-    save()
+    write(firstNote);
+    save();
 
-    expect(textarea.value).to.equal(empty)
-  })
+    write(secondNote);
+    save();
 
-  it('should save multiples notes', ()=> {
-    const firstNote = "first note"
-    const secondNote = "second note"
-    
-    write(firstNote)
-    save()
-    
-    write(secondNote)
-    save()
-    
-    const notes = document.querySelectorAll("div")
-    expect(notes[0].innerHTML).to.equal(firstNote)
-    expect(notes[1].innerHTML).to.equal(secondNote)
-  })
-
+    const notes = getSavedNotes();
+    expect(notes).to.contain(firstNote);
+    expect(notes).to.contain(secondNote);
+    expect(getElementByText(firstNote)).to.not.equal(undefined);
+    expect(getElementByText(secondNote)).to.not.equal(undefined);
+  });
 });
 
-
 const write = async (text) => {
-  const textarea = document.querySelector('textarea') 
+  const textarea = document.querySelector('textarea');
 
-  await userEvent.default.type(textarea, text)
-}
+  await userEvent.default.type(textarea, text);
+};
 
-const save = async() => {
-  const textarea = document.querySelector('textarea') 
+const save = async () => {
+  const textarea = document.querySelector('textarea');
 
-  await userEvent.default.type(textarea,"{enter}")
-}
+  await userEvent.default.type(textarea, '{enter}');
+};
 
 const getSavedNotes = () => {
-  return JSON.parse(window.localStorage.getItem("notes"))
-}
+  return JSON.parse(window.localStorage.getItem('notes'));
+};
+
+const getElementByText = (text) => {
+  const body = document.querySelector('body');
+  let foundElement;
+  body.childNodes.forEach((element) => {
+    if (element.textContent === text) foundElement = element;
+  });
+  return foundElement;
+};
