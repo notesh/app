ceddca7908d30c508e40ca346f3ea6c51d90a33c
EvaNuezG <evanuez24@gmail.com>
2022-03-28 18:52:08 +0200

    Add note cypress test

--- cypress/integration/addNote_spec.js
+++ cypress/integration/addNote_spec.js
@@ -1,5 +1,6 @@
 describe('add a note', () => {
-    it('Visits wadus app', () => {
+    it('Visits wadus app and write a note', () => {
       cy.visit('http://localhost:8080/')
-    })
+      cy.get('#InputNote').type('My first note').type('{enter}')
+    });
   })
