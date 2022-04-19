8cc04817f357d47c2dc88868fa18d4f6ca279935
EvaNuezG <evanuez24@gmail.com>
2022-04-19 15:06:59 +0200

    remove console logs

--- server.js
+++ server.js
@@ -5,22 +5,18 @@ import fs from "fs"
 const PORT = 8080;
 
 var style = fs.readFileSync('output.css', 'utf8', function read(err, data) {
-    console.log(err)
     if (err) {
         throw err;
     }
 });
 
 var index = fs.readFileSync('index.html', 'utf8', function read(err, data) {
-    console.log(err)
     if (err) {
         throw err;
     }
 });
 
 http.createServer(function (req, res) {
-    console.log(req.url)
-
     res.setHeader('Content-Type', 'text/html')
     res.statusCode = 200
     if (req.url === '/') {
