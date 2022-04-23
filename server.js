61187a6fbde23d351f0ef254d4205ce506f25f51
EvaNuezG <evanuez24@gmail.com>
2022-04-23 19:16:04 +0200

    move code to js file

--- server.js
+++ server.js
@@ -16,12 +16,23 @@ var index = fs.readFileSync('index.html', 'utf8', function read(err, data) {
     }
 });
 
+var script = fs.readFileSync('script.js', 'utf8', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+});
+
 http.createServer(function (req, res) {
     res.setHeader('Content-Type', 'text/html')
     res.statusCode = 200
+    console.log(req.url)
     if (req.url === '/') {
         res.setHeader('Content-Type', 'text/html')
         res.write(index);
+    }
+    if (req.url === '/script.js') {
+        res.setHeader('Content-Type', 'text/javascript')
+        res.write(script);
     }
     if (req.url === '/output.css') {
         res.setHeader('Content-Type', 'text/css')
