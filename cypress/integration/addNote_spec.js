0b991d8229e7028d2e49d164da85b132285070b5
Mario Sanz <mariosanz92@gmail.com>
2022-05-18 10:58:01 +0200

    added assert in cypress test

--- cypress/integration/addNote_spec.js
+++ cypress/integration/addNote_spec.js
@@ -1,6 +1,9 @@
 describe('add a note', () => {
-    it('Visits wadus app and write a note', () => {
-      cy.visit('http://localhost:8080/')
-      cy.get('#InputNote').type('My first note').type('{enter}')
-    });
-  })
+  it('Visits wadus app and write a note', () => {
+    cy.visit('http://localhost:8080/');
+
+    cy.get('#InputNote').type('My first note').type('{enter}');
+
+    cy.get('div').should('contain', 'My first note');
+  });
+});
