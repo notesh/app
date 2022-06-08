1c75cca89f094cdeaa1b8b8b08ae69607424449c
Vicente Baixauli <vicente.baixauli@qatium.com>
2022-06-08 16:50:05 +0200

    Format files

--- src/note.js
+++ src/note.js
@@ -1,20 +1,20 @@
 class Note {
-    constructor(window){
+    constructor(window) {
         this.localstorage = window.localStorage
         this.document = window.document
     }
 
-    render(){
+    render() {
         const enterKey = 13
         const container = this.document.getElementById("container")
         const textarea = this.document.createElement("textarea")
         textarea.setAttribute("id", "InputNote");
         textarea.setAttribute("type", "text");
         textarea.setAttribute("class", "my-6 w-2/3 border rounded border-teal-600 focus:border focus:border-2 focus:border-teal-600 focus:outline-0")
-        
+
         container.appendChild(textarea)
         const notes = JSON.parse(this.localstorage.getItem("notes")) || []
-        
+
         textarea.addEventListener("keydown", (event) => {
             if (event.keyCode === enterKey) {
                 createNote(textarea.value)
@@ -24,13 +24,13 @@ class Note {
                 event.preventDefault()
             }
         })
-        
+
         const getNotes = () => {
             notes.forEach(note => {
                 createNote(note)
             })
         }
-        
+
         const createNote = text => {
             const div = this.document.createElement("div")
             div.setAttribute("data-testid", "note")
@@ -38,11 +38,11 @@ class Note {
             div.innerHTML = text
             container.appendChild(div)
         }
-        
+
         const storeNote = () => {
             this.localstorage.setItem("notes", JSON.stringify(notes))
         }
-        
+
         getNotes()
     }
 }
