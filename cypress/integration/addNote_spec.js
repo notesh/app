1f93227442155e77e88c28bde64c8e44893bbab3
Vicente Baixauli <vibaiher@proton.me>
2022-06-08 17:23:52 +0200

    Update e2e tests

--- cypress/integration/addNote_spec.js
+++
@@ -1,9 +0,0 @@
-describe('add a note', () => {
-  it('Visits wadus app and write a note', () => {
-    cy.visit('http://localhost:8080/');
-
-    cy.get('#InputNote').type('My first note').type('{enter}');
-
-    cy.get('div').should('contain', 'My first note');
-  });
-});
